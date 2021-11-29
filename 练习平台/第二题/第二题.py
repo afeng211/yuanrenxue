66b4abd384a8710ff9cb2ac43a829904750e935b
w_work <1277808631@qqcom>
2021-11-29 22:07:07 +0800

    change

--- 练习平台/第二题/第二题.py
+++ 练习平台/第二题/第二题.py
@@ -1,5 +1,18 @@
 import requests, execjs
+from settings.settings import PRACTICE_ID
 
-url = ""
+session = requests.session()
+url = "https://www.python-spider.com/challenge/2"
 
+cookies = {
+    "sessionid":PRACTICE_ID
 
+}
+
+#res = session.get(url=url, cookies=cookies)
+
+with open("two.js", "r")as f:
+    js_text = f.read()
+    compile = execjs.compile(js_text)
+    cookie = compile.call("SDK")
+    print(cookie.split(";")[0].replace("sign=", ""))
